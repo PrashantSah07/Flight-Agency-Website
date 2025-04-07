e2311835bb911c82f6357f5f5d6f0a485dab4d24
prashant <prashantkumar855114@gmail.com>
2025-04-07 16:14:52 +0530

    images changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,25 +11,25 @@ import Footer from './components/Footer.jsx'
 
 const array = [
     {
-        place: '/src/assets/paris.avif',
+        place: 'https://images.unsplash.com/photo-1628221680019-f28a2716e727?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDN8fHNpbmdhcG9yZXxlbnwwfHwwfHx8MA%3D%3D',
         traveler: '/src/assets/travelers3.avif',
         username: "Emma Carter",
         socialMediaHandle: "@emmacrt"
     },
     {
-        place: '/src/assets/america.avif',
+        place: 'https://images.unsplash.com/photo-1619187269972-267d2b78a423?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGhvbmclMjBrb25nfGVufDB8fDB8fHww',
         traveler: '/src/assets/travelers2.avif',
         username: 'Wilson Lindsey',
         socialMediaHandle: '@wilsonhndsey'
     },
     {
-        place: '/src/assets/dubaii.avif',
+        place: 'https://images.unsplash.com/photo-1524562865630-b991c6c2f261?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGF1c3RyYWxpYXxlbnwwfHwwfHx8MA%3D%3D',
         traveler: '/src/assets/travelers1.avif',
         username: "Liam Johnson",
         socialMediaHandle: "@liamjnsn"
     },
     {
-        place: '/src/assets/dubai.avif',
+        place: 'https://plus.unsplash.com/premium_photo-1664304458186-9a67c1330d02?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Z29hfGVufDB8fDB8fHww',
         traveler: '/src/assets/travelers4.avif',
         username: "Noah Bennett",
         socialMediaHandle: "@noahbntt"
